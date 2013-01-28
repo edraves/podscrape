1d96d69bdafbdc0aa674d64943d364bab46864a0
Eric Draves <edraves20@gmail.com>
2013-01-28 16:01:30 -0600

    Driver now iterates through subgenres
    Driver also can now populate the current top level genre when the
    currently selected genre is a subgenre. It will traverse upward and
    choose the subgenres parent.

--- podscrape/driver.py
+++ podscrape/driver.py
@@ -12,17 +12,49 @@ class Driver:
     def populate_state(self):
         scraper = self.fetcher.fetch(self.start)
         self.genres = scraper.get_top_level_genre_tags()
+        self.subgenres = scraper.get_subgenre_tags()
 
         #Loop through to see which tag has selected in the class
-        for tag in self.genres:
-            if "selected" in tag['class']:
-                self.current_genre = tag
-                break
+        self.current_subgenre = self.get_currently_selected_subgenre()
+        self.current_genre = self.get_currently_selected_genre()
 
         #We want to slice out the preceding genres, so we start in the
         #right place. Add one to remove the current genre from the list
+        #TODO: If currently selected is a subgenre, remove all prior 
+        #top level genres
         current_index = self.genres.index(self.current_genre) + 1
         self.genres[0:current_index] = []
+
+        if self.current_subgenre:
+            current_index = self.subgenres.index(self.current_subgenre) + 1
+            self.subgenres[0:current_index] = []
+
+    def get_currently_selected_genre(self):
+        selected = None
+        if self.genres:
+            for tag in self.genres:
+                if "selected" in tag['class']:
+                    selected = tag
+                    break
+    # If the current genre is a subgenre, we want to continue forward from
+    # its parent later
+            else:
+                subgenre = self.get_currently_selected_subgenre()
+                if subgenre:
+                    parent_li = subgenre.parent.parent.parent
+                    selected = parent_li.find("a", class_="top-level-genre")
+        return selected
+
+    def get_currently_selected_subgenre(self):
+        selected = None
+        if self.subgenres:
+            parent_list = self.subgenres[0].parent.parent
+            selected = parent_list.find("a", class_="selected")
+#            for tag in self.subgenres:
+#                if "selected" in tag['class']:
+#                    selected = tag
+#                    break
+        return selected
 #first cycle through pages, then letters, then subgenres, then move to the
 #next genre.
 #Remember that the first page of any letter is no page at all, to scrape the
@@ -39,6 +71,13 @@ class Driver:
         else:
             self.current_genre = None
         return self.current_genre
+
+    def next_subgenre(self):
+        if (self.subgenres):
+            self.current_subgenre = self.subgenres.pop(0)
+        else:
+            self.current_subgenre = None
+        return self.current_subgenre
 
 #parse out current letter, and page.
 def parse_url(url):
--- tests/driver_tests.py
+++ tests/driver_tests.py
@@ -6,13 +6,20 @@ from podscrape.driver import Driver, parse_url
 test_url = "http://itunes.apple.com/genre/podcasts-arts/id1301?mt=2"
 test_url2 = "http://itunes.apple.com/genre/podcasts-arts/id1301?mt=2&letter=A"
 test_url3 = "https://itunes.apple.com/us/genre/podcasts-society-culture/id1324?mt=2&letter=N&page=2#page"
+test_url4 = "https://itunes.apple.com/us/genre/podcasts-music/id1310?mt=2"
+test_url5 = "https://itunes.apple.com/us/genre/podcasts-arts-food/id1306?mt=2"
+
 test_url_file = "./tests/itunes_arts_page.html"
 test_url2_file = "./tests/itunes_arts_page_letter_a.html"
 test_url3_file = "./tests/itunes_society_and_culture_n_2.html"
+test_url4_file = "./tests/itunes_music_page.html"
+test_url5_file = "./tests/itunes_arts_food_page.html"
 fetch_values = {
     test_url: test_url_file,
     test_url2: test_url2_file,
-    test_url3: test_url3_file
+    test_url3: test_url3_file,
+    test_url4: test_url4_file,
+    test_url5: test_url5_file
 }
 
 def test_parse_url():
@@ -79,6 +86,48 @@ def test_next_genre_from_middle():
     driver = Driver(test_url3, MockFetcher(fetch_values))
     genre = driver.next_genre()
     assert_equal(genre.text, "Sports & Recreation")
+
+def test_next_subgenre():
+    driver = Driver(test_url, MockFetcher(fetch_values))
+    assert_equal(driver.current_genre.string, "Arts")
+    assert_equal(driver.current_subgenre, None)
+
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Design")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Fashion & Beauty")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Food")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Literature")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Performing Arts")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Visual Arts")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre, None)
+
+def test_next_subgenre_music():
+    driver = Driver(test_url4, MockFetcher(fetch_values))
+    assert_equal(driver.current_genre.string, "Music")
+    assert_equal(driver.current_subgenre, None)
+
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre, None)
+
+def test_next_subgenre_from_middle():
+    driver = Driver(test_url5, MockFetcher(fetch_values))
+    assert_equal(driver.current_genre.string, "Arts")
+    assert_equal(driver.current_subgenre.string, "Food")
+
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Literature")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Performing Arts")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre.text, "Visual Arts")
+    subgenre = driver.next_subgenre()
+    assert_equal(subgenre, None)
 
 """
 Driver notes
